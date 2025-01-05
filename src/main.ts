d7c5c65ef775a33f82515f936f028d0fdd9bb139
Dmitry Gorbachev <dgorbachev@interprocom.ru>
2025-01-05 21:30:01 +0300

    feat: auth & user & prisma modules

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,40 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import cookieParser from 'cookie-parser'
 import { ValidationPipe } from '@nestjs/common'
+import IORredis from 'ioredis'
+import session from 'express-session'
+import { ms, StringValue } from './libs/common/utils/ms.util'
+import { parseBoolean } from './libs/common/utils/parse-boolean.util'
+import { RedisStore } from 'connect-redis'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const config = app.get(ConfigService)
+  const redis = new IORredis(config.getOrThrow<string>('REDIS_URI'))
 
   app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
+  app.use(
+    session({
+      secret: config.getOrThrow<string>('SESSION_SECRET'),
+      name: config.getOrThrow('SESSION_NAME'),
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+        maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+        httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
+        secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
+        sameSite: 'lax',
+      },
+      store: new RedisStore({
+        client: redis,
+        prefix: config.getOrThrow<string>('SESSION_FOLDER'),
+      }),
+    }),
+  )
 
   app.enableCors({
     origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
