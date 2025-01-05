23c016fa97f90cc09645d6f539be6b9f8b6f09cd
Dmitry Gorbachev <dgorbachev@interprocom.ru>
2025-01-06 00:19:03 +0300

    feat: user module

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
+import { Prisma } from '../../prisma/__generated__'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  public constructor(private readonly prisma: PrismaService) {}
+
+  public async findById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+      include: { accounts: true },
+    })
+
+    if (!user) {
+      throw new NotFoundException('User not found')
+    }
+
+    return user
+  }
+  public async findByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+      include: { accounts: true },
+    })
+
+    if (!user) {
+      throw new NotFoundException('User not found')
+    }
+
+    return user
+  }
+
+  public async create(data: Prisma.UserCreateInput) {
+    return await this.prisma.user.create({
+      data,
+      select: {
+        email: true,
+        displayName: true,
+        picture: true,
+      },
+    })
+  }
+}
